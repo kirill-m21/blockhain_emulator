d7a75000a540733d96c69c9f6d27ca924ac5abb6
kirill.m <kirill.m@blaize.tech>
2022-01-14 18:25:47 +0200

    delete trash

--- blockchain_network/src/header.rs
+++
@@ -1,7 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-
-#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
-pub struct Header {
-    pub timestamp: String,
-    pub nonce: u128,
-}
--- blockchain_network/src/transaction.rs
+++
@@ -1,8 +0,0 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-
-#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
-pub struct Transaction {
-    pub from: String,
-    pub to: String,
-    pub amount: u64,
-}
